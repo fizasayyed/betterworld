f19480598fbd5cac737a4fb6a6151fcd2497e546
Fiza Sayyed <fizaasayyed7@gmail.com>
2024-08-29 16:56:55 +0530

    Fund help others text added

--- frontend/src/components/imagearea/imagearea.tsx
+++ frontend/src/components/imagearea/imagearea.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image"
-import { AspectRatio } from "@/components/ui/aspect-ratio"
-import { Button } from "../ui/button"
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Button } from "../ui/button";
 
 export default function ImageArea() {
     return (
@@ -10,10 +10,19 @@ export default function ImageArea() {
                     src="/images/donate1.jpg"
                     alt="Donation photo"
                     fill
-                    className="h-full w-full object-cover" />
+                    className="h-full w-full object-cover"
+                />
             </AspectRatio>
-            <h1 className="absolute bottom-20 left-10 text-9xl text-white font-extrabold">Fund</h1>
-            <Button size="xl" className="absolute bottom-10 right-10">Start Donating Today</Button>
+            <div className="absolute bottom-5 left-5 flex items-end">
+                <h1 className="text-[250px] leading-none text-white font-extrabold">Fund</h1>
+                <div className="flex flex-col ml-3 mb-5">
+                    <h1 className="text-6xl text-white leading-none font-bold">Help</h1>
+                    <h1 className="text-6xl text-white leading-none font-bold">Others</h1>
+                </div>
+            </div>
+            <Button size="xl" className="absolute bottom-10 right-10">
+                Start Donating Today
+            </Button>
         </div>
-    )
+    );
 }
