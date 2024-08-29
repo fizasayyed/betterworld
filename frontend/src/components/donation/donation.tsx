36aefa2913a8a7891a5ff63cc7dda32121513958
Fiza Sayyed <fizaasayyed7@gmail.com>
2024-08-29 18:15:27 +0530

    FAQ section added

--- frontend/src/components/donation/donation.tsx
+++ frontend/src/components/donation/donation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import {
@@ -10,6 +11,12 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function Donation() {
     return (
@@ -19,7 +26,7 @@ export default function Donation() {
                     Select Your Donation
                 </p>
             </div>
-            <div className="flex justify-between mt-8 w-full max-w-8xl">
+            <div className="flex justify-between px-10 mt-8 w-full max-w-8xl">
                 {[10, 25, 50, 100].map((amount) => (
                     <Card key={amount} className="w-[350px]">
                         <CardHeader>
@@ -41,6 +48,31 @@ export default function Donation() {
                         </CardFooter>
                     </Card>
                 ))}
+            </div>
+            <div className="w-full max-w-8xl mt-10 px-20">
+                <Accordion type="single" collapsible className="font-semibold text-2xl mt-20 w-full max-w-8xl">
+                    <p className="w-full text-4xl mb-2 fonts-extrabold text-gray-700">Frequently Asked</p>
+                    <p className="w-full text-4xl mb-5 fonts-extrabold text-gray-700">Questions.</p>
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>How can I make donations?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It adheres to the WAI-ARIA design pattern.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Will my Donation be Used?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other
+                            components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Can I setup a recurring Donation?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It's animated by default, but you can disable it if you prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
             </div>
         </div>
     );
