2f28ce124e6f2a571f32f7840e9c6f95945bc66e
Fiza <fizaasayyed7@gmail.com>
2024-09-19 13:32:08 +0530

    API endpoint changed to deployed backend URL

--- frontend/src/lib/api/endpoint.ts
+++ frontend/src/lib/api/endpoint.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    // hostname: process.env.NEXT_PUBLIC_API_DOMAIN,
-    hostname: "http://localhost:5000",
+    hostname: process.env.NEXT_PUBLIC_API_DOMAIN,
+    // hostname: "http://localhost:5000",
 
     endpoint: {
         testAPI: 'api/test',
